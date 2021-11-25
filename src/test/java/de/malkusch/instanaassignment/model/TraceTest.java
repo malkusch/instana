80d7fdb9fa4fa8cce622a1dedaf234a7ee2f61f8
Markus Malkusch <markus@malkusch.de>
2021-11-25 20:53:39 +0100

    Fix

--- src/test/java/de/malkusch/instanaassignment/model/TraceTest.java
+++ src/test/java/de/malkusch/instanaassignment/model/TraceTest.java
@@ -2,16 +2,15 @@ package de.malkusch.instanaassignment.model;
 
 import static java.util.Arrays.stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class TraceTest {
 
     @Test
-    public void shouldParseSingle() {
-        var trace = Trace.parse("A");
-
-        assertEquals(trace("A"), trace);
+    public void shouldFailParsingSingle() {
+        assertThrows(IllegalArgumentException.class, () -> Trace.parse("A"));
     }
 
     @Test
@@ -19,6 +18,13 @@ public class TraceTest {
         var trace = Trace.parse("A-B");
 
         assertEquals(trace("A", "B"), trace);
+    }
+
+    @Test
+    public void shouldParseTriplet() {
+        var trace = Trace.parse("A-B-C");
+
+        assertEquals(trace("A", "B", "C"), trace);
     }
 
     private static Trace trace(String... services) {
